23ceb1dc53fac52f22eaabf37d5528ce2b89f3d8
Lupin_1107 <amitkumar104203@gmail.com>
2025-03-06 10:58:59 +0530

    [Amit] Add. display image using property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
+  imageUrl = '../assets/BridgeLabz.avif';
+
+  ngOnInit(): void {
+    this.title = 'Hello from Bridgelabz'
+  }
 }
