d5c4f1552fdf7961e8454285ee90468c694b5f88
Lupin_1107 <amitkumar104203@gmail.com>
2025-03-06 12:25:51 +0530

    [Amit] Add. ensure userName is valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
  title = 'Helloworld';
  imageUrl = '../assets/BridgeLabz.avif';
  url = "https://www.bridgelabz.com";
- userName: string = "";
+  userName: string = "";
+  nameError: string = "";
   
  ngOnInit(): void {
    this.title = 'Hello from Bridgelabz';
@@ -18,5 +19,22 @@ export class AppComponent {
  onClick($event:Event) {
    console.log("Save button is clicked!", $event);
    window.open(this.url, "_blank");
- }
+  }
+  
+  onInput($event: any) {
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\s]{2,}$/;
+    
+    if (!this.userName) {
+      this.nameError = ""; 
+      return;
+    }
+  
+    if (nameRegex.test(this.userName)) {
+      this.nameError = ""; 
+    } else {
+      this.nameError = "Name is Incorrect"; 
+    }
+  }
+  
 }
