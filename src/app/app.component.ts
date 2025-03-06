ffa954be45d29f0d06c6dc74b6c0140d9568304e
Lupin_1107 <amitkumar104203@gmail.com>
2025-03-06 11:51:14 +0530

    [Amit] Add. display user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+ title = 'Helloworld';
+ imageUrl = '../assets/BridgeLabz.avif';
+ url = "https://www.bridgelabz.com";
+ userName: string = "";
+  
+ ngOnInit(): void {
+   this.title = 'Hello from Bridgelabz';
+ }
 
-
-  ngOnInit(): void{
-    this.title ='Hello from Bridgelabz'
-  }
+ onClick($event:Event) {
+   console.log("Save button is clicked!", $event);
+   window.open(this.url, "_blank");
+ }
 }
